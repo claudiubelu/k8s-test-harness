c8a5bfed0f8b4d9843559d4be241bb1cfd09fa76
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2024-08-05 19:39:16 +0300

    Minor additions to the util.docker_util module. (#30)
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- k8s_test_harness/util/docker_util.py
+++ k8s_test_harness/util/docker_util.py
@@ -3,22 +3,39 @@
 # See LICENSE file for licensing details
 #
 
+import logging
 import subprocess
 from typing import List
 
+LOG = logging.getLogger(__name__)
 
-def ensure_image_contains_paths(image, paths):
+
+def ensure_image_contains_paths(
+    image: str, paths: List[str], override_entrypoint: str = ""
+):
     """Ensures the given container image contains the provided paths
     within it by attempting to run the image and 'ls' all of them.
     """
-    base_cmd = ["docker", "run", "--rm", "--entrypoint", "ls", image, "-l"]
+    base_cmd = ["docker", "run", "--rm", "--entrypoint"]
+    entrypoint = "ls"
+    args = ["-l"]
+    if override_entrypoint:
+        entrypoint = override_entrypoint
+        args = ["ls", "-l"]
+    base_cmd.append(entrypoint)
+    base_cmd.append(image)
+    base_cmd.extend(args)
     base_cmd.extend(paths)
 
+    LOG.debug(f"Running command: {base_cmd}")
     subprocess.run(base_cmd, check=True)
 
 
 def list_files_under_container_image_dir(
-    image: str, root_dir: str = "/", exclude_hidden_files: bool = True
+    image: str,
+    root_dir: str = "/",
+    override_entrypoint: str = "",
+    exclude_hidden_files: bool = True,
 ) -> List[str]:
     """Lists all regular file paths under the given dir in the given image by
     attempting to run the image and executing `find -type f` within the dir.
@@ -28,16 +45,22 @@ def list_files_under_container_image_dir(
         "run",
         "--rm",
         "--entrypoint",
-        "find",
-        image,
-        root_dir.rstrip("/"),
-        "-type",
-        "f",
     ]
+    if root_dir != "/":
+        root_dir = root_dir.rstrip("/")
+    entrypoint = "find"
+    args = [root_dir, "-type", "f"]
+    if override_entrypoint:
+        entrypoint = override_entrypoint
+        args = ["find", root_dir, "-type", "f"]
+    cmd.append(entrypoint)
+    cmd.append(image)
+    cmd.extend(args)
 
     if exclude_hidden_files:
         cmd.extend(["-not", "-path", "'*/\\.*'", "(", "!", "-iname", ".*", ")"])
 
+    LOG.debug(f"Running command: {cmd}")
     proc = subprocess.run(cmd, check=True, capture_output=True)
 
     return [line.decode("utf8").strip() for line in proc.stdout.splitlines()]
